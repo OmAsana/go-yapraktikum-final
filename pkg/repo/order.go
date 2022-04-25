52ff16a78dc05ac0255a9d36822b8b1209b5580e
Stepan Ardalin <5485713+OmAsana@users.noreply.github.com>
2022-04-25 12:53:06 +0300

    Update repo

--- pkg/repo/order.go
+++ pkg/repo/order.go
@@ -26,47 +26,6 @@ func newOrderRepo(db *sql.DB, logger *zap.Logger) *orderRepo {
 	return &orderRepo{db: db, log: logger}
 }
 
-//func (u *orderRepo) CreateNewOrder(ctx context.Context, order models.Order) Error {
-//	var err error
-//	l := u.log.With(zap.Int("order", order.OrderID))
-//	defer func() {
-//		if err != nil {
-//			l.Error("error creating order", zap.Error(err))
-//		}
-//	}()
-//	sqlStatement := `INSERT INTO orders (order_id, status, tx_type, accrual, user_id, uploaded_at)
-//VALUES ($1, $2, $3, $4, $5, $6)`
-//
-//	res, err := u.db.ExecContext(ctx, sqlStatement,
-//		order.OrderID,
-//		models.NewStatus,
-//		order.TXType,
-//		order.Accrual,
-//		order.UserID,
-//		time.Now())
-//	if err != nil {
-//		// duplicate key error
-//		if strings.Contains(err.Error(), "SQLSTATE 23505") {
-//			return ErrDuplicateOrder
-//		}
-//	}
-//
-//	inserts, err := res.RowsAffected()
-//	if err != nil {
-//		return ErrInternalError
-//
-//	}
-//
-//	if inserts != 1 {
-//		// TODO
-//		// Implement  zapcore.ObjectMarshaler
-//		u.log.Debug("Did not create order", zap.Reflect("order", order))
-//		return ErrInternalError
-//	}
-//
-//	return nil
-//}
-
 func (u *orderRepo) CreateNewOrder(ctx context.Context, order models.Order) Error {
 	l := u.log.With(zap.Int("order", order.OrderID))
 
