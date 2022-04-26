5355b24c5b1d5f30978285bb98871c2ef3e4b1f3
Stepan Ardalin <5485713+OmAsana@users.noreply.github.com>
2022-04-26 17:17:37 +0300

    Add order controller

--- pkg/controllers/order.go
+++ pkg/controllers/order.go
@@ -1,1 +1,29 @@
 package controllers
+
+import (
+	"strconv"
+	"time"
+
+	"github.com/OmAsana/go-yapraktikum-final/pkg/models"
+)
+
+type Order struct {
+	Number     string             `json:"number"`
+	Status     models.OrderStatus `json:"status"`
+	Accrual    int                `json:"accrual,omitempty"`
+	UploadedAt string             `json:"uploaded_at"`
+}
+
+func OrderModelToController(mo models.Order) Order {
+	o := Order{
+		Number:     strconv.Itoa(mo.OrderID),
+		Status:     mo.Status,
+		UploadedAt: mo.UploadedAt.Format(time.RFC3339),
+	}
+
+	if mo.Status != models.InvalidStatus {
+		o.Accrual = mo.Accrual
+	}
+
+	return o
+}
